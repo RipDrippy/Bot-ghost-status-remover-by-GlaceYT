f609f3c48eec1ae441ba859ea8d403540e24a491
RipDrippy <ripdrippy8@gmail.com>
2024-05-14 19:54:26 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to @Rip_Drippy"];
+const statusMessages = ["Listening to @Rip_Drippy", "Listening to @Rip_Drippy"];
 
 
 let currentIndex = 0;
